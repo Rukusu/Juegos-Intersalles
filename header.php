a8dcf4d6240453b0f5ab20c2985663aa97201a3c
Rukusu <nesesitounos@hotmail.com>
2019-10-03 10:37:15 -0500

    Added Header Option

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 					<a href="universidades.php">Universidades Participantes</a>
 					<a href="resultados.php">Rol de Juegos y Resultados</a>
 					<a href="reglamentos.php">Reglamentos</a>
+					<a href="prontuario.php">Prontuario</a>
 					<div id="social">
 						<a href="https://twitter.com/lasallelaguna" target="_blank" style="background-image: url(images/twitter.png)"></a>
 						<a href="https://www.facebook.com/Red-La-Salle-México-2760673140824619/" target="_blank" style="background-image: url(images/facebook.png)"></a>
