a94c373433886a4e6dbb20a3a23afbc6d74a4afe
Rukusu <nesesitounos@hotmail.com>
2019-11-07 01:13:37 -0600

    Quil front working

--- blog/pirata.php
+++ blog/pirata.php
@@ -17,31 +17,68 @@
 		<link rel="stylesheet" type="text/css" href="../css/hospitales.css">
 		<link rel="stylesheet" type="text/css" href="../css/juegos.css?<?php echo date('YmdHis'); ?>">
 		<link rel="stylesheet" type="text/css" href="../css/interior.css?<?php echo date('YmdHis'); ?>">
+		<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
 		
-		<script src="js/quill/quill.js"></script>		
+		<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+	
 		<script type="text/javascript" src="../../js/jquery.js"></script>
 		<script type="text/javascript" src="../js/juegos.js?<?php echo date('YmdHis'); ?>"></script>
 	</head>
 	<body>
-		<script>
-			var quill = new Quill('#editor-container', {
-				modules: {
-					toolbar: [
-						[{ header: [1, 2, false] }],
-						['bold', 'italic', 'underline'],
-						['image', 'code-block']
-					]
-				},
-				placeholder: 'Compose an epic...',
-				theme: 'snow'  // or 'bubble'
-			});
-		</script>
+		<!-- Initialize Quill editor -->
 		<?php include('../header_int.php'); ?>
 		<div id="titulo">
 			<div class="wrapper">NOTICIAS</div>
 		</div>
 		<section class="wrapper" id="items" style="margin-top: 20px">
-			<div id="editor-container"></div>
+			<form>
+			<p>Titulo</p>
+			<input type="text" id="titulo_noticia">
+
+			<p>Cuerpo</p>
+			<!-- Create the editor container -->
+			<div id="editor"></div>
+
+			<button type="button" id="send_noticia">Guardar</button>
+
+			</form>
+			<script>
+				var toolbarOptions = [
+					['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+					['blockquote', 'code-block'],
+
+					[{ 'header': 1 }, { 'header': 2 }],               // custom button values
+					[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+					[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+					[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+					[{ 'direction': 'rtl' }],                         // text direction
+
+					[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+					[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+					[ 'link', 'image', 'video', 'formula' ],          // add's image support
+					[{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+					[{ 'font': [] }],
+					[{ 'align': [] }],
+
+					['clean']                                         // remove formatting button
+				];
+
+			var quill = new Quill('#editor', {
+				modules: {
+					toolbar: toolbarOptions
+				},
+
+				theme: 'snow'
+			});
+			
+			document.getElementById("send_noticia").onclick = function(){
+			var myEditor = document.querySelector('#editor');
+			var titulo_noticia = document.getElementById("titulo_noticia").value;
+			var text = myEditor.children[0].innerHTML;
+			console.log(titulo_noticia);
+			console.log(text);
+			}
+		</script>
 		</section>
 		<?php include('../footer.php'); ?>
 	</body>
